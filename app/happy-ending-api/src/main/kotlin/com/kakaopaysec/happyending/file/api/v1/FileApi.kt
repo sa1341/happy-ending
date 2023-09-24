f4b9ef7a5da6d64ed569a2246303a549a2389614
임준영 <a79007714@gmail.com>
2023-09-24 23:44:07 +0900

    chore: byteArray 방식으로 파일 다운로드 예제코드 작성

--- app/happy-ending-api/src/main/kotlin/com/kakaopaysec/happyending/file/api/v1/FileApi.kt
+++ app/happy-ending-api/src/main/kotlin/com/kakaopaysec/happyending/file/api/v1/FileApi.kt
@@ -3,14 +3,16 @@ package com.kakaopaysec.happyending.file.api.v1
 import com.kakaopaysec.happyending.file.service.FileDownloader
 import jakarta.servlet.http.HttpServletResponse
 import mu.KotlinLogging
+import org.apache.commons.io.FileUtils
 import org.springframework.core.io.Resource
-import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpHeaders.CONTENT_DISPOSITION
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.io.File
 
 private val log = KotlinLogging.logger {}
 
@@ -30,7 +32,26 @@ class FileApi(
         val contentType = MediaType.APPLICATION_OCTET_STREAM_VALUE
         return ResponseEntity.ok()
             .contentType(MediaType.parseMediaType(contentType))
-            .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"${resource.filename}\"")
+            .header(CONTENT_DISPOSITION, "attachment; filename=\"${resource.filename}\"")
             .body(resource)
+    }
+
+    @GetMapping("/binary")
+    fun downloadByteArrays(
+        @RequestParam("fileName") fileName: String,
+        httpServletResponse: HttpServletResponse
+    ) {
+        log.info { "fileName: $fileName" }
+        val files = FileUtils.readFileToByteArray(File("/Users/limjun-young/workspace/privacy/dev/test/plaintext.txt"))
+        val contentType = MediaType.APPLICATION_OCTET_STREAM_VALUE
+
+        httpServletResponse.contentType = contentType
+        httpServletResponse.setContentLength(files.size)
+        httpServletResponse.setHeader(CONTENT_DISPOSITION, "attachment; filename=\"plain.txt\"")
+        httpServletResponse.setHeader("Content-Transfer-Encoding", "binary")
+
+        httpServletResponse.outputStream.write(files)
+        httpServletResponse.outputStream.flush()
+        httpServletResponse.outputStream.close()
     }
 }
