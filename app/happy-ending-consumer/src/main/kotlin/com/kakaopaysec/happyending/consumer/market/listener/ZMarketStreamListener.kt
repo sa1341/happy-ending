9ecd7590254cf4c225d510ac6b31f2374699a8de
임준영 <a79007714@gmail.com>
2023-10-03 14:49:04 +0900

    study: KStream에서 제공하는 branch 메서드를 활용하여 스트림별로 관심사 토픽으로 분기처리 예제코드 작성
    * ZMarket에서 판매하는 커피, 전자제품과 같은 카테고리 명으로 predicate를 제공하여 고객들의 제품 선호도 같은 데이터들을 스트림을 활용하여 각 토픽별로 분기처리를 해봤음(branch()가 deprecated 된 상태여셔.. split()을 먼저 사용하는 것을 권고하고 있음.. 생각보다 구글링에 자료가 많이 없어서 분석을 통해서 일단 작성하였습니다.)

--- app/happy-ending-consumer/src/main/kotlin/com/kakaopaysec/happyending/consumer/market/listener/ZMarketStreamListener.kt
+++ app/happy-ending-consumer/src/main/kotlin/com/kakaopaysec/happyending/consumer/market/listener/ZMarketStreamListener.kt
@@ -5,6 +5,7 @@ import com.kakaopaysec.happyending.consumer.market.model.PurchasePattern
 import com.kakaopaysec.happyending.consumer.market.model.RewardAccumulator
 import org.apache.kafka.common.serialization.Serdes
 import org.apache.kafka.streams.StreamsBuilder
+import org.apache.kafka.streams.kstream.Branched
 import org.apache.kafka.streams.kstream.Consumed
 import org.apache.kafka.streams.kstream.KStream
 import org.apache.kafka.streams.kstream.Produced
@@ -26,6 +27,8 @@ class ZMarketStreamListener {
             Consumed.with(Serdes.String(), Serdes.serdeFrom(purchaseJsonSerdes.first, purchaseJsonSerdes.second))
         ).mapValues { p: Purchase ->
             Purchase.from(p)
+        }.filter { key, purchase ->
+            purchase.price > 5.00
         }
 
         val purchasePatternSerdes = createPurchasePatternJsonSerdes()
@@ -49,7 +52,40 @@ class ZMarketStreamListener {
             Produced.with(Serdes.String(), Serdes.serdeFrom(purchaseJsonSerdes.first, purchaseJsonSerdes.second))
         )
 
+        // KStream에서 제공하느 branch를 사용하여 스트림별로 관심사 토픽으로 분기처리 적용
+        divideCategoryStream(purchaseKStream, purchaseJsonSerdes)
+
         return purchaseKStream
+    }
+
+    private fun divideCategoryStream(
+        purchaseKStream: KStream<String, Purchase>,
+        jsonSerdes: Pair<JsonSerializer<Purchase>, JsonDeserializer<Purchase>>
+    ) {
+        val isCoffee = { key: String?, p: Purchase -> p.department == "coffee" }
+        val isElectronics = { key: String?, p: Purchase -> p.department == "electronics" }
+
+        val kStreamByDept = purchaseKStream
+            .split()
+            .branch(
+                isCoffee,
+                Branched.withConsumer { ks ->
+                    ks.to(
+                        "coffee",
+                        Produced.with(Serdes.String(), Serdes.serdeFrom(jsonSerdes.first, jsonSerdes.second))
+                    )
+                }
+            )
+            .branch(
+                isElectronics,
+                Branched.withConsumer { ks ->
+                    ks.to(
+                        "electronics",
+                        Produced.with(Serdes.String(), Serdes.serdeFrom(jsonSerdes.first, jsonSerdes.second))
+                    )
+                }
+            )
+            .noDefaultBranch()
     }
 
     private fun createPurchaseJsonSerdes(): Pair<JsonSerializer<Purchase>, JsonDeserializer<Purchase>> {
