f52d32635018ab237cee0b5841658c0f5422bad9
임준영 <a79007714@gmail.com>
2023-10-14 15:02:22 +0900

    feature: coffe, electronics branch 스트림을 join 스트림으로 처리하는 토폴로지 기능 추가

--- app/happy-ending-consumer/src/main/kotlin/com/kakaopaysec/happyending/consumer/market/listener/ZMarketStreamJoinerListener.kt
+++ app/happy-ending-consumer/src/main/kotlin/com/kakaopaysec/happyending/consumer/market/listener/ZMarketStreamJoinerListener.kt
@@ -1,86 +1,74 @@
 package com.kakaopaysec.happyending.consumer.market.listener
 
 import com.kakaopaysec.happyending.consumer.config.KafkaJacksonConfig
+import com.kakaopaysec.happyending.consumer.market.joiner.PurchaseJoiner
+import com.kakaopaysec.happyending.consumer.market.model.CorrelatedPurchase
 import com.kakaopaysec.happyending.consumer.market.model.Purchase
-import com.kakaopaysec.happyending.consumer.market.model.PurchasePattern
-import com.kakaopaysec.happyending.consumer.market.model.RewardAccumulator
 import org.apache.kafka.common.serialization.Serdes
+import org.apache.kafka.streams.KeyValue
 import org.apache.kafka.streams.StreamsBuilder
-import org.apache.kafka.streams.kstream.Branched
 import org.apache.kafka.streams.kstream.Consumed
+import org.apache.kafka.streams.kstream.JoinWindows
 import org.apache.kafka.streams.kstream.KStream
-import org.apache.kafka.streams.kstream.Produced
+import org.apache.kafka.streams.kstream.Printed
+import org.apache.kafka.streams.kstream.StreamJoined
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.support.serializer.JsonDeserializer
 import org.springframework.kafka.support.serializer.JsonSerializer
+import java.time.Duration
 
 @Configuration class ZMarketStreamJoinerListener {
 
     @Bean
     fun joinerPurchaseKStream(streamsBuilder: StreamsBuilder): KStream<String, Purchase> {
         val purchaseJsonSerdes = KafkaJacksonConfig.createPurchaseJsonSerdes()
+
+        val customerIdCreditCardMasking: (String?, Purchase) -> KeyValue<String, Purchase> = { key: String?, value: Purchase ->
+            val masked = Purchase.from(value)
+            KeyValue(masked.customerId, masked)
+        }
+
         val purchaseKStream = streamsBuilder.stream(
             "transactions",
             Consumed.with(Serdes.String(), Serdes.serdeFrom(purchaseJsonSerdes.first, purchaseJsonSerdes.second))
-        ).mapValues { p: Purchase ->
-            Purchase.from(p)
-        }.filter { key, purchase ->
-            purchase.price > 5.00
-        }
+        ).map(customerIdCreditCardMasking)
 
-        val purchasePatternSerdes = KafkaJacksonConfig.createPurchasePatternJsonSerdes()
-        val patternKStream = purchaseKStream
-            .mapValues { p: Purchase -> PurchasePattern.from(p) }
-            .to(
-                "patterns",
-                Produced.with(Serdes.String(), Serdes.serdeFrom(purchasePatternSerdes.first, purchasePatternSerdes.second))
-            )
-
-        val rewardAccumulatorSerdes = KafkaJacksonConfig.createRewardAccumulatorJsonSerdes()
-        val rewardsKStream = purchaseKStream
-            .mapValues { p: Purchase -> RewardAccumulator.from(p) }
-            .to(
-                "rewards",
-                Produced.with(Serdes.String(), Serdes.serdeFrom(rewardAccumulatorSerdes.first, rewardAccumulatorSerdes.second))
-            )
-
-        purchaseKStream.to(
-            "purchases",
-            Produced.with(Serdes.String(), Serdes.serdeFrom(purchaseJsonSerdes.first, purchaseJsonSerdes.second))
-        )
-
-        // KStream에서 제공하느 branch를 사용하여 스트림별로 관심사 토픽으로 분기처리 적용
         divideCategoryStream(purchaseKStream, purchaseJsonSerdes)
         return purchaseKStream
     }
 
     private fun divideCategoryStream(
         purchaseKStream: KStream<String, Purchase>,
-        jsonSerdes: Pair<JsonSerializer<Purchase>, JsonDeserializer<Purchase>>
+        purchaseJsonSerdes: Pair<JsonSerializer<Purchase>, JsonDeserializer<Purchase>>
     ) {
+        val COFFEE_PURCHASE = 0
+        val ELECTRONICS_PURCHASE = 1
+
         val isCoffee = { key: String?, p: Purchase -> p.department == "coffee" }
         val isElectronics = { key: String?, p: Purchase -> p.department == "electronics" }
 
-        val kStreamByDept = purchaseKStream
-            .split()
-            .branch(
-                isCoffee,
-                Branched.withConsumer { ks ->
-                    ks.to(
-                        "coffee",
-                        Produced.with(Serdes.String(), Serdes.serdeFrom(jsonSerdes.first, jsonSerdes.second))
-                    )
-                }
+        val branchesStream = purchaseKStream
+            .selectKey { key, value -> value.customerId }
+            .branch(isCoffee, isElectronics)
+
+        val coffeeStream = branchesStream[COFFEE_PURCHASE]
+        val electronicsStream = branchesStream[ELECTRONICS_PURCHASE]
+
+        val purchaseJoiner = PurchaseJoiner()
+        val twentyMinuteWindow = JoinWindows.ofTimeDifferenceWithNoGrace(Duration.ofMillis(60 * 1000 * 20))
+
+        val joinedKStream = coffeeStream.join(
+            electronicsStream,
+            purchaseJoiner,
+            twentyMinuteWindow,
+            StreamJoined.with(
+                Serdes.String(),
+                Serdes.serdeFrom(purchaseJsonSerdes.first, purchaseJsonSerdes.second),
+                Serdes.serdeFrom(purchaseJsonSerdes.first, purchaseJsonSerdes.second)
             )
-            .branch(
-                isElectronics,
-                Branched.withConsumer { ks ->
-                    ks.to(
-                        "electronics",
-                        Produced.with(Serdes.String(), Serdes.serdeFrom(jsonSerdes.first, jsonSerdes.second))
-                    )
-                }
-            )
+        )
+
+        joinedKStream.print(Printed.toSysOut<String?, CorrelatedPurchase?>().withLabel("joined KStream"))
     }
 }
