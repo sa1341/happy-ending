13fdcbf86e0013b97ece56cc2620e6f3ec187d7a
임준영 <a79007714@gmail.com>
2023-10-25 23:11:17 +0900

    study: redis sorted set 자료구조 저장 및 조회 예제코드 작성

--- app/happy-ending-api/src/test/kotlin/com/kakaopaysec/happyending/redis/RedisTemplateTest.kt
+++ app/happy-ending-api/src/test/kotlin/com/kakaopaysec/happyending/redis/RedisTemplateTest.kt
@@ -1,6 +1,7 @@
 package com.kakaopaysec.happyending.redis
 
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
@@ -26,5 +27,33 @@ class RedisTemplateTest @Autowired constructor(
 
         // then
         actual shouldBe userId
+    }
+
+    @Test
+    fun `redis sorted set 타입을 저장 및 조회한다`() {
+        // given
+        val key = "userRank"
+        val zSet = redisTemplate.opsForZSet()
+
+        // when
+        val expected = hashSetOf<Any>()
+        expected.add("a")
+        expected.add("b")
+        expected.add("c")
+        expected.add("d")
+        expected.add("e")
+        expected.add("f")
+
+        zSet.add(key, "d", 4.toDouble())
+        zSet.add(key, "c", 3.toDouble())
+        zSet.add(key, "a", 1.toDouble())
+        zSet.add(key, "b", 2.toDouble())
+        zSet.add(key, "f", 6.toDouble())
+        zSet.add(key, "e", 5.toDouble())
+        val range = zSet.range(key, 0, 6)
+
+        // then
+        range shouldNotBe null
+        range!!.size shouldBe 6
     }
 }
